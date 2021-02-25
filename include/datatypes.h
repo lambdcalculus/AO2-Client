47ad5b45a4a35f7c64b375a81e878a8da2c15ac8
Leifa♥ <date.epoch@gmail.com>
2021-02-25 05:14:01 +0100

    Resolving undefined method

--- include/datatypes.h
+++ include/datatypes.h
@@ -15,7 +15,11 @@ struct record_type
   bool music = false;
 
   record_type() = default;
-  record_type(QString p_name, QString p_line, QString p_color, bool p_is_system, bool p_is_music);
+  record_type(QString p_name, QString p_line, QString p_color, bool p_is_system, bool p_is_music)
+      : name(p_name), line(p_line), system(p_is_system), music(p_is_music)
+  {
+    Q_UNUSED(p_color);
+  }
 };
 
 typedef std::shared_ptr<record_type> record_type_ptr;
