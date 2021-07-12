1a351f2216e399a00478a74718788f43703fcb81
TrickyLeifa <date.epoch@gmail.com>
2021-07-12 18:04:25 +0200

    Reworked frame handling for AOMovie to be more accurate

--- include/aomovie.h
+++ include/aomovie.h
@@ -14,7 +14,10 @@ public:
   AOMovie(QWidget *p_parent, AOApplication *p_ao_app);
   ~AOMovie();
 
+  bool is_play_once();
   void set_play_once(bool p_play_once);
+
+  void play_file_name(QString file);
   void play(QString p_file, QString p_char = "");
   void play_interjection(QString p_char_name, QString p_interjection_name);
   void restart();
@@ -29,11 +32,12 @@ private:
   AOApplication *ao_app = nullptr;
 
   QMovie *m_movie = nullptr;
-
-  bool is_play_once = true;
+  bool m_first_loop = false;
+  bool m_play_once = true;
 
 private slots:
   void frame_change(int n_frame);
+  void handle_state(QMovie::MovieState);
 };
 
 #endif // AOMOVIE_H
