12108ce3322fca2ba6ac02853966ad623c3f7cbe
TrickyLeifa <date.epoch@gmail.com>
2021-10-26 22:18:57 +0200

    Added -noscaling argument option
    Resolve #244

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@ int main(int argc, char *argv[])
 {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
   // High-DPI support is for Qt version >=5.6.
-  // However, many Linux distros still haven't brought their stable/LTS
-  // packages up to Qt 5.6, so this is conditional.
   AOApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+  for (int i = 0; i < argc; ++i)
+  {
+    const QString l_arg(argv[i]);
+
+    if (l_arg == "-noscaling")
+    {
+      QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, false);
+      QCoreApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+    }
+  }
 #endif
 
 #ifdef Q_OS_MACOS
