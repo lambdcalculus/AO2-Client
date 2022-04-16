2289aa08204d44e112f5456e2edc6e5668558d2d
TrickyLeifa <date.epoch@gmail.com>
2022-04-16 16:02:12 +0200

    Added website link

--- src/drmediatester.cpp
+++ src/drmediatester.cpp
@@ -22,7 +22,8 @@ void DRMediaTester::p_check_status(QMediaPlayer::MediaStatus p_status)
   case QMediaPlayer::InvalidMedia:
     call_warning("Your operating system appears to not currently support video playback of <b>MP4</b> files. The video "
                  "playback will not work properly.<br /><br />In order for the feature to function properly, you "
-                 "will need to install the appropriate codecs.");
+                 "will need to install the appropriate codecs.<br /><br />For more information visit "
+                 "<a href=\"https://www.danganronpaonline.com\">https://www.danganronpaonline.com</a>");
     break;
 
   default:
